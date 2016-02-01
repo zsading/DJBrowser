782ea580d1407afac72e073e79019203cb223d35
zsading <dingjiatiancai@163.com>
2016-02-01 14:25:45 +0800

    初始化项目

---
+++ DJBrowser/DJBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJBrowser
+//
+//  Created by yoanna on 16/2/1.
+//  Copyright © 2016年 yoanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DJBrowser/DJBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DJBrowser
+//
+//  Created by yoanna on 16/2/1.
+//  Copyright © 2016年 yoanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
